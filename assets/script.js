42f3ca99c969905a7f890ed6878fe661e62623f1
Gabriel <sernat243@gmail.com>
2023-07-05 13:59:52 -0600

    created array called quizData containing questions, choices, and answers properties

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var quizData = [
+    {
+        question: "Commonly used Data types DO Not include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: 2
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with __________.",
+        choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+        answer: 2
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _________.",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: 3
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal/Bash", "for Loops", "console.log"],
+        answer: 3
+    }
+];
